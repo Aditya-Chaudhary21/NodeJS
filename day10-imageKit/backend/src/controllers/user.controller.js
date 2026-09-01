51496f96c72e2471943dbd5c3c546050b116255a
lakshay8088 <lakshaychaudhary006@gmail.com>
2026-09-02 00:02:41 +0530

    imageKit

--- day10-imageKit/backend/src/controllers/user.controller.js
+++ day10-imageKit/backend/src/controllers/user.controller.js
@@ -1,23 +1,20 @@
 const userModel = require("../models/user.model");
 const uploadFile = require("../services/storage.service");
 
-const testApi = async(req, res) => {
+const testApi = async (req, res) => {
   try {
-    
     const file = req.file;
-    console.log("file===================",file);
+    console.log("file===================", file);
     if (!file) {
       return res.status(400).json({
         message: "No file uploaded",
       });
     }
 
-    const imageUrl =  await uploadFile(file.buffer, file.originalname);
+    const imageUrl = await uploadFile(file.buffer, file.originalname);
     const post = await userModel.create({
       image: imageUrl.url,
-    
     });
-
 
     return res.status(201).json({
       message: "file tranfered successfully",
@@ -25,7 +22,7 @@ const testApi = async(req, res) => {
   } catch (error) {
     return res.status(500).json({
       message: "Internal Server Error",
-       error: error.message,
+      error: error.message,
     });
   }
 };
